892da62564a0b9a200d7043d9e7d0caae2de5c7d
Leomister1233 <152848011+Leomister1233@users.noreply.github.com>
2024-05-21 02:02:52 +0800

    erhsjh

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:3000"],
     method:["POST","GET"],
     credentials:true
 }));
