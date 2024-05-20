9810111fa369a6dccc4ef21b4a93ec95ce55bc0d
Leomister1233 <152848011+Leomister1233@users.noreply.github.com>
2024-05-20 15:12:52 +0800

    third-commit

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(cors())
 app.use(express.json())//important for sending data
 app.use(express.urlencoded({ limit: "25mb" }));
 
+app.get('/',(req,res)=>{
+    res.send('Express Server is working')
+})
 
 app.listen(8804,() => {
     console.log("listening on")
