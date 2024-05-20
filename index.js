17fdde50ca758d711417833f523aabccee3373f0
Leomister1233 <152848011+Leomister1233@users.noreply.github.com>
2024-05-21 02:03:46 +0800

    dgj

--- index.js
+++ index.js
@@ -28,10 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-    method:["POST","GET"],
-    credentials:true
-}));
+app.use(cors());
 
 // MySQL Database Connection
 const db = mysql.createConnection({
